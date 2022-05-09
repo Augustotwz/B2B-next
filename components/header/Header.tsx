33bd09ada4ce4d1a0962fbe9a8055a17b37e2153
Augusto <augusto.cesar.99@hotmail.com>
2022-05-08 22:29:47 -0300

    feat: add header / navbar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,45 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { IconMailHeader } from '../../icons'
+import styles from './Header.module.css'
+
 export function Header() {
-  return <header>Hello header</header>
+  const links = [
+    { name: 'FEATURES', href: '/' },
+    { name: 'PRICING', href: '/' },
+    { name: 'SERVICES', href: '/' },
+    { name: 'PARTNERS', href: '/' },
+  ]
+
+  return (
+    <header>
+      <nav className={styles.nav}>
+        <div className={styles.brand}>
+          <Link href='/'>
+            <a>
+              <Image
+                src={IconMailHeader}
+                alt='NinjaMail'
+                width={84}
+                height={42}
+              />
+              <span>NinjaMail</span>
+            </a>
+          </Link>
+        </div>
+        <div className={styles.navList}>
+          <ul className={styles.ul}>
+            {links.map(({ name, href }) => (
+              <li key={name}>
+                <Link href={href}>
+                  <a>{name}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <button className={styles.btn}>SIGN UP FREE</button>
+        </div>
+      </nav>
+    </header>
+  )
 }
--- components/page-template/page-template.tsx
+++ components/page-template/page-template.tsx
@@ -1,14 +1,15 @@
 import { Header } from '../header/Header'
+import { Footer } from '../footer/Footer'
 import { PageProps } from './types'
 
 export function PageTemplate({ ...props }: PageProps) {
   return (
-    <>
+    <div style={{ margin: 'auto' }}>
       <Header />
       <main>
         <div>{props.children}</div>
       </main>
-      Footer
-    </>
+      <Footer />
+    </div>
   )
 }
