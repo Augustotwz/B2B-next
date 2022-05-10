425b1fb2451a03cc861282e9867d96d04ae6678a
Augusto <augusto.cesar.99@hotmail.com>
2022-05-09 21:26:22 -0300

    feat: add section-2

---
+++ components/section-2/Section-2.tsx
@@ -0,0 +1,12 @@
+import styles from './Section-2.module.css'
+
+export function SectionTwo() {
+  return (
+    <div className={styles.background}>
+      <div className={styles.content}>
+        <h1>Reach More Customers</h1>
+        <button>LEARN HOW</button>
+      </div>
+    </div>
+  )
+}
